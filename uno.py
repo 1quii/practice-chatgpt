ddff177ced5c5cfd5ea0d37f8bc1a0284952f72f
1quii <diego187hv@gmail.com>
2026-03-23 13:19:25 +0100

    first commit

---
+++ uno.py
@@ -0,0 +1,15 @@
+mensaje="Hello, World!"
+
+print(mensaje)
+
+mensaje="""esto es un mensaje
+multilinea
+con varias lineas"""
+print(mensaje)
+
+numero1=10
+numero2=20
+if numero1>numero2:
+    print("numero1 es mayor que numero2")
+else:
+    print("numero2 es mayor que numero1")
